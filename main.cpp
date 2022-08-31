a7717784db0232a6632055d84793f383eff0b0be
diixo <anatolii.podoba@gmail.com>
2022-09-01 02:39:53 +0300

    - Fix eternal loop bug
    - Small refactoring
    
    Bugs still exist

--- main.cpp
+++ main.cpp
@@ -54,6 +54,24 @@ const wchar_t eu_lower_ext[SZ] = {
    L'\x013e', L'\x0144', L'\x0146', L'\x014d', L'\x0151', L'\x0155', L'\x0161', L'\x016b', L'\x0171', L'\x0173', L'\x01b6'
 };
 
+const wchar_t PUNCT[15] = { L'…', L',', L':', L'!', L'—', L'*', L'«', L'»', L';', L'"', L'&', L')', L'(', L'/', L'|' };
+
+const wchar_t ALLOWABLE[27] =
+{
+   L'\n',
+   L'\r',
+   L'\t',
+   L' ',
+   L'…', L',', L':', L'!', L'—', L'*', L'«', L'»', L';', L'"', L'&',
+   L'(', L')', L'/', L'|', L'{', L'}', L'[', L']', L'“', L'”', L'#',
+   L'\x00a0'
+
+   //L'+',
+   //L'>',
+   //L'<',
+};
+//////////////////////////////////////////////////////////////////////////
+
 wstring_t cstring_to_wstring(const char* c_str)
 {
    std::string cstr(c_str);
@@ -388,14 +406,20 @@ void trimming(const std::map <wstring_t, size_t>& filterMap, std::list <wstring_
    {
       wstring_t& wstr = *it;
 
-      rtrim(wstr, L"\x0022\x0027\x0028\x0029\x002d\x002a\x003a\x003b\x005b\x005d\x003f\x002e\x002f");
-      ltrim(wstr, L"\x0022\x0027\x0028\x0029\x002d\x002a\x003a\x003b\x005b\x005d\x002b");
-
-      //rtrim(wstr, L"\x0023\x0026\x0027\x0028\x0029\x002a\x002d\x002e\x002f\x003a\x003b\x003c\x003d\x003e\x003f\x005c\x007e\x00a9\x00ae\x005f");
-      //ltrim(wstr, L"\x0023\x0026\x0027\x0028\x0029\x002a\x002d\x002f\x005c\x007e\x00a9\x00ae\x005f");
-
-      if (!wstr.empty())
+      if ((wstr.size() == 1) && wcschr(ALLOWABLE, wstr.back()))
       {
+         it = outList.erase(it);
+         //++it;
+      }
+      else
+      {
+         rtrim(wstr, L"\x0022\x0027\x0028\x0029\x002d\x002a\x003a\x003b\x005b\x005d\x003f\x002e\x002f");
+         ltrim(wstr, L"\x0022\x0027\x0028\x0029\x002d\x002a\x003a\x003b\x005b\x005d\x002b");
+
+         //rtrim(wstr, L"\x0023\x0026\x0027\x0028\x0029\x002a\x002d\x002e\x002f\x003a\x003b\x003c\x003d\x003e\x003f\x005c\x007e\x00a9\x00ae\x005f");
+         //ltrim(wstr, L"\x0023\x0026\x0027\x0028\x0029\x002a\x002d\x002f\x005c\x007e\x00a9\x00ae\x005f");
+
+
          bool skip = true;
 
          wstring_t tstr = wstr;
@@ -757,46 +781,6 @@ void report(
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-const wchar_t PUNCT[16] = { L'…', L',', L':', L'!', L'?', L'—', L'*', L'«', L'»', L';', L'"', L'&', L')', L'(', L'/', L'|' };
-
-const wchar_t ALLOWABLE[30] =
-{
-L'\n',
-L'\r',
-L'\t',
-L' ',
-L';',
-L'(',
-L')',
-L'{',
-L'}',
-L'[',
-L']',
-L'*',
-L',',
-L'!',
-L'?',
-L':',
-L'/',
-//L'+',
-L'"',
-L'=',
-L'*',
-L'&',
-L'#',
-L'|',
-
-L'“',
-L'”',
-L'…',
-//L'>',
-//L'<',
-L'—',
-L'\x00a0',
-L'«',
-L'»'
-};
-
 
 void splitString(const wchar_t* buff, size_t buff_sz, std::list<wstring_t>& words)
 {
@@ -816,7 +800,7 @@ void splitString(const wchar_t* buff, size_t buff_sz, std::list<wstring_t>& word
          if (sz > 0)
          {
             wstring_t s_new(p_new, sz);
-            rtrim(s_new, L".:");
+            rtrim(s_new, L".:?");
             if (!s_new.empty())
             {
                words.push_back(s_new);
@@ -854,6 +838,9 @@ void processString(const wchar_t* buff, size_t buff_sz, const std::map <wstring_
    std::list <wstring_t> tokenList;
 
    splitString(buff, buff_sz, tokenList);
+
+   const wstring_t wstr(buff);
+   assert(buff_sz == wstr.size());
 
    //wcstok(wstr, L"\x0020\x0021\x002c\x003b\x007c", tokenList);   // " !,;|"
 
@@ -934,9 +921,9 @@ void loadFile_utf8(const char* filepath, const std::wstring& filename_out, const
 
          if (pBuff != buff)
          {
-            *pBuff = wch;
-            pBuff++;
-            str_sz++;
+            //*pBuff = wch;
+            //pBuff++;
+            //str_sz++;
             *pBuff = 0;
             pBuff = buff;
             {
@@ -1130,7 +1117,7 @@ int main(int argc, char* argv[])
    }
    else
    {
-      wprintf(L"Text-analyzer [Version 5 (c) Diixo\n");
+      wprintf(L"Text-analyzer [Version 6 (c) Diixo\n");
       if (argc == 3)
       {
          const wstring_t filterFile = cstring_to_wstring(argv[1]);
